c16e7a1fe8c7eea59034bbf5aac765e8d735dc6e
Viktor <velenic@mail.ru>
2022-06-06 14:21:05 +0300

    feat: add contact section and switch theme

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ const toggle = document.querySelector('.toggle');
 const topbar = document.querySelector('.topbar');
 const navigation = document.querySelector('.navigation');
 const main = document.querySelector('.main');
+const menuItems = document.querySelectorAll('.navigation ul li');
+const themeSwitch = document.querySelector('.theme__switch');
+const body = document.querySelector('body');
 
 toggle.onclick = function(){
   toggle.classList.toggle('active');
   topbar.classList.toggle('active');
   navigation.classList.toggle('active');
   main.classList.toggle('active');
+}
+
+const toggleMenu = () => {
+  navigation.classList.remove('active');
+  main.classList.remove('active');
+}
+
+menuItems.forEach(item => item.addEventListener('click', toggleMenu))
+
+themeSwitch.onclick = function(){
+  body.classList.toggle('dark')
 }
